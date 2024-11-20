1c0265b5a80f81e36a3dcdc6936a65b20085a53a
chiencm <chiencmph53462@gmail.com>
2024-11-20 07:44:43 +0700

    first commit

---
+++ shopToan/view/template_footer.php
@@ -0,0 +1,30 @@
+<footer>
+        <div class="footer-container">
+            <div class="footer-section about">
+                <img src="assets_user/img/anhlogo2.png" width="150px">
+                <p>Chào mừng bạn đến với cửa hàng thời trang của chúng tôi. Chúng tôi mang đến những xu hướng thời trang mới nhất và độc đáo.</p>
+                <img class="bocongthuong" src="https://www.kkfashion.vn/themes/kkfashion/assets/img//kkfashion-da-thong-bao.png" width="180px">
+            </div>
+            <div class="footer-section links">
+                <h2>Liên kết hữu ích</h2>
+                <ul>
+                    <li><a href="index.php">Trang chủ</a></li>
+                    <li><a href="#">Sản phẩm</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+            </div>
+            <div class="footer-section contact">
+                <h2>Liên hệ</h2>
+                <p><i class="fa fa-map-marker"></i> 38-Lê Đức Thọ-Nam Từ Liêm-Hà Nội</p>
+                <p><i class="fa fa-envelope"></i> chiencmph53462@gmail.com</p>
+                <p><i class="fa fa-phone"></i> +84 582198868</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; 2024 3CiBi.shop | Nhóm 6
+        </div>
+    </footer>
+</body>
+</html>
+<script src="/assets_user/js/danhmuc.js"></script>
